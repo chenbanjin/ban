432e05b347daad6b77be435f25c6d90df4999041
banjin <ibanjin@sohu.com>
2021-12-21 17:42:39 +0800

    1

---
+++ src/App/ErrorMessage.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ban\Easemob\App;
+/**
+ * Created by PhpStorm.
+ * User: banjin
+ * Date: 2021/12/7
+ * Time: 14:57 
+ */
+class ErrorMessage
+{
+
+}
---
+++ src/Facades/Easemob.php
@@ -0,0 +1,18 @@
+<?php namespace ban\Easemob\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Easemob extends Facade
+{
+
+    /**
+     *
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'EaseMob';
+    }
+}
