bdae9d95717ce3dd21474b66ceb650fc3b88dab6
banjin <ibanjin@sohu.com>
2021-12-21 14:00:05 +0800

    push

---
+++ easemob/src/EaseMobTestService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: BanJin
+ * Date: 2021/12/21
+ * Time: 11:32
+ */
+
+namespace Ban;
+
+class EaseMobTestService
+{
+
+    public function index()
+    {
+        echo "1111This is test package ...";
+    }
+}
